2c1a1038185a148df7cde562e04db6c79987970c
muhammadraflyhusen <raflyhusen311@gmail.com>
2025-10-30 08:57:41 +0700

    Menambahkan variabel state untuk menyimpan input nama, alamat, dan jenis kelamin

--- app/src/main/java/com/example/inputpengguna/Teksedit.KT.kt
+++ app/src/main/java/com/example/inputpengguna/Teksedit.KT.kt
@@ -1,2 +1,12 @@
 package com.example.inputpengguna
 
+@Composable
+fun FormDataDiri(modifier: Modifier) {
+
+    // variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
+
+
+
